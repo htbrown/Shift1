aaa220d5b989a2e15df50bf7a4214df9cab9a400
Hayden <28675646+htbrown@users.noreply.github.com>
2019-03-20 19:39:15 +0000

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -26,7 +26,7 @@ module.exports = async (client, message, args) => {
             guildchannel.send({
                 embed: {
                     title: 'Ban',
-                    description: `**${message.author.tag} (${message.author.id})** has banned **${banUser.user.username} (${banUser.id})** for **${reason}**.`,
+                    description: `${message.author.tag} (${message.author.id}) has banned ${banUser.user.username} (${banUser.id}) for ${reason}.`,
                     color: 0x36393F
                 }
             });
