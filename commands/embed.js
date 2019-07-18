a8eda6a930863f9f077c13578d5d6455bb0101aa
Hayden <nedyah.brown@gmail.com>
2019-07-18 19:57:50 +0100

    v1.2.2
    + Updated hug command
    + Added embed command

---
+++ commands/embed.js
@@ -0,0 +1,18 @@
+const package = require('../package.json');
+
+module.exports = async (client, message, args) => {
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return client.util.embed(client, message, 'You do not have permission to use this command. If this is an error, make sure you have the \'Manage Messages\' permission.', 'red');
+    if (!args) return client.util.embed(client, message, 'You need to include a message to put in the embed!', 'red');
+    message.delete();
+    message.channel.send({embed: {
+            author: {name: `Hey ${message.author.username}`, icon_url: message.author.avatarURL},
+            color: 0x36393F,
+            description: args.join(" "),
+            footer: {text: `v${package.version}`}
+    }});
+};
+
+module.exports.info = {
+    description: 'Create an embed with your choice of message.',
+    usage: 'embed [message]'
+}
--- commands/hug.js
+++ commands/hug.js
@@ -3,7 +3,7 @@ const package = require('../package.json');
 module.exports = async (client, message, args) => {
     if (!args[0]) return client.util.embed(client, message, `You, my friend, need some friends.`, undefined, undefined, undefined, undefined, undefined, {url: 'https://media1.tenor.com/images/0b76d2714be31c6b0b88eb3be1a6c4f9/tenor.gif?itemid=4367540'})
     if (args[0].id == message.author.id) return client.util.embed(client, message, `Lonely meter: 999`);
-    client.util.embed(client, message, `*Awhh!* <@${message.author.id}> just hugged ${args[0]}!`, undefined, undefined, undefined, undefined, undefined, {url: 'https://media.giphy.com/media/od5H3PmEG5EVq/giphy.gif'})
+    client.util.embed(client, message, `*Awhh!* <@${message.author.id}> just hugged ${args[0]}!`, undefined, undefined, undefined, undefined, undefined, {url: 'https://media.giphy.com/media/lXiRKBj0SAA0EWvbG/giphy.gif'})
 }
 module.exports.info = {
     description: 'Hug someone!',
